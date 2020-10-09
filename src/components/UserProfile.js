dc68adf5f97bb7297ab0434a11d2a86ae6af4085
unknown <utkarshmaheria@gmail.com>
2020-10-09 13:27:46 +0530

    check if user is a friend

--- src/components/UserProfile.js
+++ src/components/UserProfile.js
@@ -12,6 +12,20 @@ class UserProfile extends Component {
     }
   }
 
+  checkIfUserIsAFriend = () => {
+    console.log('this.props', this.props);
+    const { match, friends } = this.props;
+    const userId = match.params.userId;
+
+    const index = friends.map((friend) => friend.to_user._id).indexOf(userId);
+
+    if (index !== -1) {
+      return true;
+    }
+
+    return false;
+  };
+
   render() {
     const {
       match: { params },
@@ -24,6 +38,7 @@ class UserProfile extends Component {
       return <h1>Loading...</h1>;
     }
 
+    const isUserAFriend = this.checkIfUserIsAFriend();
     return (
       <div className="settings">
         <div className="img-container">
@@ -44,16 +59,21 @@ class UserProfile extends Component {
         </div>
 
         <div className="btn-grp">
-          <button className="button save-btn">Add Friend</button>
+          {!isUserAFriend ? (
+            <button className="button save-btn">Add Friend</button>
+          ) : (
+            <button className="button save-btn">Remove Friend</button>
+          )}
         </div>
       </div>
     );
   }
 }
 
-function mapStateToProps({ profile }) {
+function mapStateToProps({ profile, friends }) {
   return {
     profile,
+    friends,
   };
 }
 export default connect(mapStateToProps)(UserProfile);
